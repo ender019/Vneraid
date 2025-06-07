e341489fd9756f6af1f387c66aedd91c401201df
mamikulin <mamikulin@gmail.com>
2025-06-07 13:37:09 +0300

    ttt

--- src/App.js
+++ src/App.js
@@ -1,16 +1,29 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
 
 function App() {
-  const tg = window.Telegram?.WebApp;
+  const [tg, setTg] = useState(null);
+
+  useEffect(() => {
+    const webApp = window.Telegram?.WebApp;
+
+    if (webApp) {
+      webApp.ready(); // Inform Telegram the app is ready
+      setTg(webApp);
+    }
+  }, []);
 
   const sendToBot = () => {
-    tg?.sendData("Hello from React!");
+    if (tg) {
+      tg.sendData("Hello from React Mini App!");
+    } else {
+      alert("Telegram WebApp not available");
+    }
   };
 
   return (
-    <div className="App">
+    <div className="App" style={{ padding: 20 }}>
       <h1>Hello, Telegram!</h1>
-      <button onClick={sendToBot}>Send to Bot</button>
+      <button onClick={sendToBot}>Send Message to Bot</button>
     </div>
   );
 }
