2b2769eec389eaa99c3df7298c6f673d27a0ffd0
mamikulin <mamikulin@gmail.com>
2025-06-07 13:23:16 +0300

    Init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
 function App() {
+  const tg = window.Telegram?.WebApp;
+
+  const sendToBot = () => {
+    tg?.sendData("Hello from React!");
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello, Telegram!</h1>
+      <button onClick={sendToBot}>Send to Bot</button>
     </div>
   );
 }
---
+++ src/pages/home.js
@@ -0,0 +1,18 @@
+import React, { useState } from 'react';
+
+
+import List from '../components/list';
+import bottom from '../components/bottom';
+
+const Home = () => {
+    return(
+        <>
+            <div className='profile_info'>
+                hello
+            </div>
+            {/* <list /> */}
+        </>
+    );
+};
+
+export default Home
